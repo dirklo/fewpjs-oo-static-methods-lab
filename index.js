05b9782cdc4e9eac333cec1f1f882703364bd3dd
Rick <dirklo@gmail.com>
2021-03-21 15:14:14 +0100

    commit tree 67c3a72c34fdd326b3c7829f80a29cb91aeb493a

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
 class Formatter {
-  //add static methods here
+
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    let res = ''
+    for (let char of string) {
+      debugger;
+      if (/^[a-zA-Z-'' ']/.test(char)){
+        res += char
+      }
+    }
+    return res
+  }
+
+  static titleize(string) {
+    const noCap = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    string = Formatter.sanitize(string);
+    const words = string.split(" ");
+    let res = [];
+    for (let i = 0; i < words.length; i++) {
+      if (!noCap.includes(words[i]) || i === 0) {
+        res.push(Formatter.capitalize(words[i]));
+      } else {
+        res.push(words[i])
+      };
+    };
+    return res.join(" ")
+  }
 }
